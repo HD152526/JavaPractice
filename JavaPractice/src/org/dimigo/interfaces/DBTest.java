103a927b1b270f2384c87d651e02f64d09a52ed9
lee <lee@DESKTOP-I6J8GD2>
2017-05-24 15:50:22 +0900

    DB가 바뀌었어요

---
+++ JavaPractice/src/org/dimigo/interfaces/DBTest.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package org.dimigo.interfaces;
+
+/**
+ *<pre>
+ * org.dimigo.interfaces
+ *   |_ DBTest
+ * 
+ * 1. 개요 :
+ * 2. 작성일 : 2017. 5. 24.
+ * </pre>
+ *
+ * @author 		: lee
+ * @version 	: 1.0
+ */
+public class DBTest {
+
+	public static void main(String[] args) {
+		IDBManager s = IDBManager.getDBObject(IDBManager.SYBASE_DATABASE);
+		System.out.println("<< 변경 전 >>");
+		crud(s);
+		System.out.println();
+		IDBManager o = IDBManager.getDBObject(IDBManager.ORACLE_DATABASE);
+		System.out.println("<< 변경 후 >>");
+		crud(o);
+	}
+	private static void crud(IDBManager db){
+		db.insert();
+		db.search();
+		db.update();
+		db.delete();
+	}
+	
+}
---
+++ JavaPractice/src/org/dimigo/interfaces/IDBManager.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package org.dimigo.interfaces;
+
+/**
+ *<pre>
+ * org.dimigo.interfaces
+ *   |_ IDBManager
+ * 
+ * 1. 개요 :
+ * 2. 작성일 : 2017. 5. 24.
+ * </pre>
+ *
+ * @author 		: lee
+ * @version 	: 1.0
+ */
+public interface IDBManager {
+
+	String ORACLE_DATABASE = "ORACLE";
+	String SYBASE_DATABASE = "SYBASE";
+	
+	void insert();
+	void search();
+	void update();
+	void delete();
+	
+	static IDBManager getDBObject(String database){
+		if(ORACLE_DATABASE.equals(database)){
+			return new OracleDB();
+		}
+		else if(SYBASE_DATABASE.equals(database)){
+			return new SybaseDB();
+		}
+		return null;
+	}
+	
+}
