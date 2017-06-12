8fc4f440778914f8287f9d46e08c82ad1eebe7ab
lee <lee@DESKTOP-I6J8GD2>
2017-06-12 14:11:07 +0900

    멜론 차트

--- JavaPractice/src/org/dimigo/collection/MelonGenreChart.java
+++ JavaPractice/src/org/dimigo/collection/MelonGenreChart.java
@@ -24,29 +24,24 @@ public class MelonGenreChart {
 
 	public static void main(String[] args) {
 		Map<String, List<Music>> map = new HashMap<>();
-		List<Music> list = new ArrayList<Music>();
-		List<Music> list2 = new ArrayList<Music>();
-		Music[] music = {
-				new Music("I LUV IT", "PSY"),
-				new Music("맞지?", "언니쓰"),
-				new Music("SIGNAL", "트와이스"),
-				new Music("팔레트", "아이유")
-		};
-		map.put("[발라드]", list);
-		map.put("[댄스]", list2);
-		list.add(music[3]);
-		list2.add(music[0]);
-		list2.add(music[1]);
+		List<Music> balladlist = new ArrayList<Music>();
+		List<Music> dancelist = new ArrayList<Music>();
+	
+		map.put("[발라드]", balladlist);
+		map.put("[댄스]", dancelist);
+		balladlist.add(new Music("팔레트", "아이유"));
+		dancelist.add(new Music("I LUV IT", "PSY"));
+		dancelist.add(new Music("맞지?", "언니쓰"));
 		
-		System.out.println("-- << 멜론 장르별 챠트 >> --");
+		System.out.println("-- << 멜론 장르별 챠트 >> --");//lkjb  nn
 		printMap(map);
 		
 		System.out.println("-- << 댄스 2위 곡 변경 >> --");
-		list2.set(1, music[2]);
+		dancelist.set(1, new Music("SIGNAL", "트와이스"));
 		printMap(map);
 		
 		System.out.println("-- << 댄스 1위 곡 삭제 >> --");
-		list2.remove(0);
+		dancelist.remove(0);
 		printMap(map);
 		
 		System.out.println("-- << 전체 리스트 삭제 >> --");
